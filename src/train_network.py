0248cf4d1f186fb637ffe83f06abf3831ffde32e
nilbog <naztechmedia@gmail.com>
2025-10-17 21:26:17 -0500

    Modularized training and validation loops in training class

--- src/train_network.py
+++ src/train_network.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import torch
@@ -10,11 +11,13 @@ class PokerEquityNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(53, 64),
+            nn.Linear(53, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
             nn.ReLU(),
             nn.Linear(64, 32),
             nn.ReLU(),
-            nn.Linear(32, 1),
+            nn.Linear(32, 1)
         )
 
     def forward(self, x):
@@ -23,7 +26,7 @@ class PokerEquityNN(nn.Module):
 
 
 class TrainPokerEquity():
-    def __init__(self, data, batch_size, epochs, output):
+    def __init__(self, data, batch_size, epochs, load, output):
         print("Initialized training class")
         self.device = self.get_device()
         self.batch_size = batch_size
@@ -34,14 +37,69 @@ class TrainPokerEquity():
 
         self.model = PokerEquityNN().to(self.device)
         self.criterion = nn.BCELoss()
-        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-3)
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=5e-6)
 
-        self.model = train_model(self.model, self.training_data,
-                                 self.validation_data, self.criterion,
-                                 self.optimizer, self.device, self.epochs)
+        self.epoch = 0
+        self.loss = 0
 
-        torch.save(self.model.state_dict(), self.output)
+        self.training_loss = []
+        self.validation_loss = []
+
+        self.model = self.train_model()
+
+        torch.save({
+            'epoch': self.epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'loss': self
+        }, self.output)
         print(f"Training complete. Model saved to {self.output}")
+
+    def train_model(self):
+        for epoch in tqdm(range(self.epochs), desc="Training Epochs"):
+            self.training_loop()
+            self.validation_loop()
+            self.epoch = epoch
+
+        plot_data(self.training_loss, self.validation_loss)
+
+        return self.model
+
+    def training_loop(self):
+        self.model.train()
+        running_loss = 0.0
+        for inputs, targets in self.training_data:
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+
+            self.optimizer.zero_grad()
+            outputs = self.model(inputs).squeeze(-1)
+            loss = self.criterion(outputs, targets)
+            loss.backward()
+            self.optimizer.step()
+
+            self.loss = loss.item()
+            running_loss += loss.item()
+
+        avg_loss = running_loss / len(self.training_data)
+
+        self.training_loss.append(avg_loss)
+
+    def validation_loop(self):
+        self.model.eval()
+        with torch.no_grad():
+            running_loss = 0.0
+            for inputs, targets in self.validation_data:
+                inputs, targets = inputs.to(
+                    self.device), targets.to(self.device)
+
+                outputs = self.model(inputs).squeeze(-1)
+                loss = self.criterion(outputs, targets)
+
+                running_loss += loss.item()
+
+        avg_loss = running_loss / len(self.validation_data)
+
+        self.validation_loss.append(avg_loss)
 
     def get_device(self):
         device = torch.accelerator.current_accelerator(
@@ -73,48 +131,6 @@ def plot_data(training_loss, validation_loss):
     plt.show()
 
 
-def train_model(model, training_data, validation_data,
-                criterion, optimizer, device, epochs):
-    training_loss = []
-    validation_loss = []
-    for epoch in tqdm(range(epochs), desc="Training Epochs"):
-        # Training loop
-        model.train()
-        running_loss = 0.0
-        for inputs, targets in training_data:
-            inputs, targets = inputs.to(device), targets.to(device)
-
-            optimizer.zero_grad()
-            outputs = model(inputs).squeeze(-1)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-        # Validation loop
-        model.eval()
-        with torch.no_grad():
-            val_loss = 0
-            for inputs, targets in validation_data:
-                inputs, targets = inputs.to(device), targets.to(device)
-
-                outputs = model(inputs).squeeze(-1)
-                loss = criterion(outputs, targets)
-
-                val_loss += loss.item()
-
-        avg_loss = running_loss / len(training_data)
-        avg_val_loss = val_loss / len(validation_data)
-
-        training_loss.append(avg_loss)
-        validation_loss.append(avg_val_loss)
-
-    plot_data(training_loss, validation_loss)
-
-    return model
-
-
 def main():
     parser = argparse.ArgumentParser(
         description='Train poker equity neural network'
@@ -125,13 +141,31 @@ def main():
                         help='Batch size (default: 32)')
     parser.add_argument('-e', '--epochs', type=int, default=50,
                         help='Number of training itterations (default: 50)')
-    parser.add_argument('-o', '--output', type=str,
-                        default='models/default.pth',
-                        help='Output filepath (default: models/default.pth)')
+    parser.add_argument('-l', '--load', type=str,
+                        help='Optional: Include model checkpoint')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Output filename (saved in data/*.pth)')
 
     args = parser.parse_args()
 
-    TrainPokerEquity(args.data, args.batch_size, args.epochs, args.output)
+    load = args.load
+
+    output = f"models/{args.output}.pth"
+    o = Path(output)
+    if o.exists():
+        print(f"Caution: {output} already exists.")
+        i = input("Would you like to overwrite the file? y/N\n")
+        if i == "n" or i == "N" or "":
+            filename = input(
+                "Enter new filename:\n")
+            output = f"models/{filename}.pth"
+        elif i == "y" or i == "Y":
+            print(f"Overwriting {output}")
+        else:
+            print("Unknown value. Quitting...")
+            exit()
+
+    TrainPokerEquity(args.data, args.batch_size, args.epochs, load, output)
 
 
 if __name__ == "__main__":
